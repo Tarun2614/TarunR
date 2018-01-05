ee4999fc778a80284f28e099ce41548a696c0cd2
Tarun2614 <35129267+Tarun2614@users.noreply.github.com>
2018-01-05 11:36:48 +0530

    Create 1.c

---
+++ Wayne/1.c
@@ -0,0 +1,22 @@
+#TarunR
+
+#include <stdio.h>
+
+int main(void) {
+int a;
+printf("Enter the number\n");
+scanf("%d",&a);
+if(a>0)
+{
+	printf("Positive\n");
+	}
+	else if(a<0)
+	{
+		printf("Negative/n");
+	}
+else
+{
+	printf("Zero\n");
+}
+return 0;
+}
